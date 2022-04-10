2221a62466721476ba25054b19e5334c725a3545
tajinder046 <95207787+tajinder046@users.noreply.github.com>
2022-04-10 23:52:32 +0530

    Add files via upload

---
+++ EmployeeService/target/classes/my.sql
@@ -0,0 +1,3 @@
+ insert into roles values('1','ADMIN');
+ INSERT INTO USERS VALUES('1','$2a$12$Lv7hswag3CvUd1KzhFtOKeX2bZdkEqr7TeAo9W.gY2kVyM0DBBV/e','user1');
+ insert into users_roles values(1,1);
